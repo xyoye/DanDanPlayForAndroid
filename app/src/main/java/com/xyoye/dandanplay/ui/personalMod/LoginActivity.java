df3d5e1532a39193433faf63eb3a0adc02f1f2cd
Bo <shine_5402@live.cn>
2018-08-03 12:55:52 +0800

    登录界面修改完成

--- app/src/main/java/com/xyoye/dandanplay/ui/personalMod/LoginActivity.java
+++ app/src/main/java/com/xyoye/dandanplay/ui/personalMod/LoginActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.text.InputType;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -33,32 +37,38 @@ import butterknife.ButterKnife;
  * Created by YE on 2018/7/22.
  */
 
-public class LoginActivity extends BaseActivity<LoginPresenter> implements LoginView,View.OnClickListener {
+public class LoginActivity extends BaseActivity<LoginPresenter> implements LoginView, View.OnClickListener {
     @BindView(R.id.login_return_iv)
     ImageView loginReturnIv;
     @BindView(R.id.user_name_et)
-    EditText userNameEt;
+    TextInputEditText userNameEt;
     @BindView(R.id.user_password_et)
-    EditText userPasswordEt;
-    @BindView(R.id.user_password_eye_iv)
-    ImageView eyeIv;
+    TextInputEditText userPasswordEt;
     @BindView(R.id.login_bt)
     Button loginBt;
     @BindView(R.id.forgot_password_tv)
     TextView forgotPasswordTv;
     @BindView(R.id.register_tv)
     TextView registerTv;
+    @BindView(R.id.user_password_layout)
+    TextInputLayout userPasswordLayout;
+    @BindView(R.id.user_name_layout)
+    TextInputLayout userNameLayout;
 
     private boolean isPShow = false;
+
     @Override
     public void initView() {
-
+        //定义全屏参数
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        //设置当前窗体为全屏显示
+        Window window = getWindow();
+        window.setFlags(flag, flag);
     }
 
     @Override
     public void initListener() {
         loginReturnIv.setOnClickListener(this);
-        eyeIv.setOnClickListener(this);
         loginBt.setOnClickListener(this);
         forgotPasswordTv.setOnClickListener(this);
         registerTv.setOnClickListener(this);
@@ -77,20 +87,9 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.login_return_iv:
                 launchMain();
-                break;
-            case R.id.user_password_eye_iv:
-                if (!isPShow){
-                    userPasswordEt.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    eyeIv.setImageResource(R.mipmap.ic_password_show);
-                    isPShow = true;
-                }else {
-                    userPasswordEt.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    eyeIv.setImageResource(R.mipmap.ic_password_hidden);
-                    isPShow = false;
-                }
                 break;
             case R.id.login_bt:
                 login();
@@ -104,14 +103,20 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
         }
     }
 
-    private void login(){
+    private void login() {
         String userName = userNameEt.getText().toString();
         String password = userPasswordEt.getText().toString();
-        if (StringUtils.isEmpty(userName)){
-            ToastUtils.showShort("用户名不能为空");
-        }else if (StringUtils.isEmpty(password)){
-            ToastUtils.showShort("密码不能为空");
-        }else {
+        if (StringUtils.isEmpty(userName)) {
+//            ToastUtils.showShort("用户名不能为空");
+            userNameLayout.setErrorEnabled(true);
+            userNameLayout.setError("用户名不能为空");
+        } else if (StringUtils.isEmpty(password)) {
+//            ToastUtils.showShort("密码不能为空");
+            userPasswordLayout.setErrorEnabled(true);
+            userNameLayout.setError("密码不能为空");
+        } else {
+            userNameLayout.setErrorEnabled(false);
+            userPasswordLayout.setErrorEnabled(false);
             presenter.login(new LoginParam(userName, password));
         }
     }
@@ -123,7 +128,7 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
 
     @Override
     public void launchMain() {
-        if(getIntent().getBooleanExtra("isOpen", false))
+        if (getIntent().getBooleanExtra("isOpen", false))
             launchActivity(MainActivity.class);
         else
             this.finish();
@@ -136,7 +141,7 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
 
     @Override
     protected void setStatusBar() {
-        StatusBarUtil.setTranslucentForImageView(this,null);
+        StatusBarUtil.setTranslucentForImageView(this, 0,null);
     }
 
     @Override

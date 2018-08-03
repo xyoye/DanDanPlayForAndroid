6c7200ce1f4c32f265ab4b43f650ae3e7645e69e
Bo <shine_5402@live.cn>
2018-08-03 08:51:22 +0800

    修改默认测试脚本

--- playerview/src/androidTest/java/com/dl7/player/ExampleInstrumentedTest.java
+++ playerview/src/androidTest/java/com/dl7/player/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.dl7.playerview.test", appContext.getPackageName());
+        assertEquals("com.dl7.player.test", appContext.getPackageName());
     }
 }

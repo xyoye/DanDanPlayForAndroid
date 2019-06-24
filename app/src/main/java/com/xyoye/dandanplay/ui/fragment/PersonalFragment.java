ba0518f1141f583c76ee6162ce196022c360da7a
xyy <Aser1127CBM>
2019-06-24 14:44:14 +0800

    个人中心UI调整

--- app/src/main/java/com/xyoye/dandanplay/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/xyoye/dandanplay/ui/fragment/PersonalFragment.java
@@ -1,25 +1,17 @@
 package com.xyoye.dandanplay.ui.fragment;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.StringUtils;
 import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.xyoye.dandanplay.R;
 import com.xyoye.dandanplay.app.IApplication;
 import com.xyoye.dandanplay.base.BaseFragment;
-import com.xyoye.dandanplay.base.BaseRvAdapter;
-import com.xyoye.dandanplay.bean.AnimeFavoriteBean;
-import com.xyoye.dandanplay.bean.PlayHistoryBean;
 import com.xyoye.dandanplay.mvp.impl.PersonalFragmentPresenterImpl;
 import com.xyoye.dandanplay.mvp.presenter.PersonalFragmentPresenter;
 import com.xyoye.dandanplay.mvp.view.PersonalFragmentView;
@@ -31,44 +23,22 @@ import com.xyoye.dandanplay.ui.activities.PersonalInfoActivity;
 import com.xyoye.dandanplay.ui.activities.PlayerSettingActivity;
 import com.xyoye.dandanplay.ui.activities.SettingActivity;
 import com.xyoye.dandanplay.ui.activities.VideoScanActivity;
-import com.xyoye.dandanplay.ui.weight.item.PersonalFavoriteAnimaItem;
-import com.xyoye.dandanplay.ui.weight.item.PersonalPlayHistoryItem;
 import com.xyoye.dandanplay.utils.AppConfig;
-import com.xyoye.dandanplay.utils.interf.AdapterItem;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.Unbinder;
 
 /**
  * Created by xyoye on 2018/6/29.
  */
 
 public class PersonalFragment extends BaseFragment<PersonalFragmentPresenter> implements PersonalFragmentView{
-    @BindView(R.id.user_info_rl)
-    RelativeLayout userInfoRl;
     @BindView(R.id.user_image_iv)
     ImageView userImageIv;
     @BindView(R.id.user_name_tv)
     TextView userNameTv;
     @BindView(R.id.button_login)
     TextView loginButton;
-    @BindView(R.id.favorite_rl)
-    RelativeLayout favoriteRl;
-    @BindView(R.id.history_rl)
-    RelativeLayout historyRl;
-    @BindView(R.id.favorite_recycler_view)
-    RecyclerView favoriteRecyclerView;
-    @BindView(R.id.history_recycler_view)
-    RecyclerView historyRecyclerView;
-    @BindView(R.id.refresh_layout)
-    SwipeRefreshLayout refresh;
-
-    private BaseRvAdapter<AnimeFavoriteBean.FavoritesBean> favoriteAdapter;
-    private BaseRvAdapter<PlayHistoryBean.PlayHistoryAnimesBean> historyAdapter;
 
     public static PersonalFragment newInstance() {
         return new PersonalFragment();
@@ -87,101 +57,39 @@ public class PersonalFragment extends BaseFragment<PersonalFragmentPresenter> im
 
     @Override
     public void initView() {
-        refresh.setColorSchemeResources(R.color.theme_color);
 
-        favoriteRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-        historyRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-
-        favoriteAdapter = new BaseRvAdapter<AnimeFavoriteBean.FavoritesBean>(new ArrayList<>()) {
-            @NonNull
-            @Override
-            public AdapterItem<AnimeFavoriteBean.FavoritesBean> onCreateItem(int viewType) {
-                return new PersonalFavoriteAnimaItem();
-            }
-        };
-        favoriteRecyclerView.setAdapter(favoriteAdapter);
-
-        historyAdapter = new BaseRvAdapter<PlayHistoryBean.PlayHistoryAnimesBean>(new ArrayList<>()) {
-            @NonNull
-            @Override
-            public AdapterItem<PlayHistoryBean.PlayHistoryAnimesBean> onCreateItem(int viewType) {
-                return new PersonalPlayHistoryItem();
-            }
-        };
-        historyRecyclerView.setAdapter(historyAdapter);
     }
 
     public void changeView() {
         if (AppConfig.getInstance().isLogin()) {
-            userInfoRl.setVisibility(View.VISIBLE);
             loginButton.setVisibility(View.GONE);
+            RequestOptions options = new RequestOptions()
+                    .error(R.mipmap.default_image)
+                    .placeholder(R.mipmap.default_image);
+
             Glide.with(this)
                     .load(AppConfig.getInstance().getUserImage())
+                    .apply(options)
                     .into(userImageIv);
             userNameTv.setText(AppConfig.getInstance().getUserScreenName());
         } else {
             loginButton.setVisibility(View.VISIBLE);
-            userInfoRl.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void initListener() {
-        refresh.setOnRefreshListener(() -> {
-            if (AppConfig.getInstance().isLogin()) {
-                presenter.getFragmentData();
-            } else {
-                refresh.setRefreshing(false);
-            }
-        });
-    }
 
-    @Override
-    public void refreshFavorite(AnimeFavoriteBean favoriteBean) {
-        favoriteAdapter.setData(favoriteBean == null
-                ? new ArrayList<>()
-                : favoriteBean.getFavorites());
-        favoriteAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void refreshHistory(PlayHistoryBean historyBean) {
-        historyAdapter.setData(historyBean == null
-                ? new ArrayList<>()
-                : historyBean.getPlayHistoryAnimes());
-        historyAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void refreshUI(AnimeFavoriteBean favoriteBean, PlayHistoryBean historyBean) {
-        favoriteAdapter.setData(favoriteBean == null
-                ? new ArrayList<>()
-                : favoriteBean.getFavorites());
-        favoriteAdapter.notifyDataSetChanged();
-
-        historyAdapter.setData(historyBean == null
-                ? new ArrayList<>()
-                : historyBean.getPlayHistoryAnimes());
-        historyAdapter.notifyDataSetChanged();
-        if (refresh != null)
-            refresh.setRefreshing(false);
     }
 
     @Override
     public void onSupportVisible() {
         if (IApplication.isUpdateUserInfo) {
-            IApplication.isUpdateUserInfo = false;
-            if (AppConfig.getInstance().isLogin()) {
-                refresh.setRefreshing(true);
-                presenter.getFragmentData();
-            } else {
-                refreshUI(null, null);
-            }
             changeView();
         }
     }
 
-    @OnClick({R.id.user_image_iv, R.id.user_info_rl, R.id.button_login, R.id.player_setting_ll, R.id.app_setting_ll, R.id.scan_setting_ll, R.id.download_setting_ll, R.id.favorite_rl, R.id.history_rl})
+    @OnClick({R.id.user_image_iv, R.id.user_info_rl, R.id.button_login, R.id.player_setting_ll, R.id.app_setting_ll, R.id.scan_setting_ll, R.id.download_setting_ll, R.id.favorite_ll, R.id.history_ll})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.player_setting_ll:
@@ -205,14 +113,14 @@ public class PersonalFragment extends BaseFragment<PersonalFragmentPresenter> im
             case R.id.user_image_iv:
                 ToastUtils.showShort("头像功能暂未开放");
                 break;
-            case R.id.favorite_rl:
+            case R.id.favorite_ll:
                 if (AppConfig.getInstance().isLogin()) {
                     launchActivity(PersonalFavoriteActivity.class);
                 } else {
                     launchActivity(LoginActivity.class);
                 }
                 break;
-            case R.id.history_rl:
+            case R.id.history_ll:
                 if (AppConfig.getInstance().isLogin()) {
                     launchActivity(PersonalHistoryActivity.class);
                 } else {

34081293a6e5809136af2be1c35c34cb41c76f12
xyoye <xieyy@anjiu-tech.com>
2021-12-14 16:44:42 +0800

    [功能优化](dev_4.0.5): 优化文件名排序规则

--- common_component/src/main/java/com/xyoye/common_component/utils/FileComparator.kt
+++ common_component/src/main/java/com/xyoye/common_component/utils/FileComparator.kt
@@ -1,6 +1,5 @@
 package com.xyoye.common_component.utils
 
-import androidx.core.text.isDigitsOnly
 import java.util.*
 import kotlin.math.min
 
@@ -27,41 +26,112 @@ class FileComparator<T>(
         val isDirectory2 = isDirectory.invoke(o2)
 
         return when {
+            //都是文件夹，或都是文件
             isDirectory1 == isDirectory2 -> {
-                compareString(o1, o2)
+                compareFileName(o1, o2)
             }
-            isDirectory1 && !isDirectory2 -> -1
+            //文件夹在前
+            isDirectory1 && isDirectory2.not() -> -1
             else -> 1
         }
     }
 
-    private fun compareString(o1: T, o2: T): Int {
-        val str1 = if(desc) value.invoke(o2) else value.invoke(o1)
-        val str2 = if(desc) value.invoke(o1) else value.invoke(o2)
-        //文字相同
-        if(str1 == str2)
+    /**
+     * 比较文件名
+     */
+    private fun compareFileName(o1: T, o2: T): Int {
+        //获取文件名
+        val str1 = if (desc) value.invoke(o2) else value.invoke(o1)
+        val str2 = if (desc) value.invoke(o1) else value.invoke(o2)
+        //完全相同的文字
+        if (str1 == str2)
             return 0
-        //字符串都是数字，转换为BigInteger比较
-        if (str1.isDigitsOnly() && str2.isDigitsOnly()){
-            return str1.toBigInteger().compareTo(str2.toBigInteger())
+
+        //尝试将整体视为Int比较
+        val intCompare = compareInt(str1, str2)
+        if (intCompare != null) {
+            return intCompare
         }
-        //提取中间差异部分
+
+        //是否存在需要对比的扩展名
+        val needCompareExt1 = isNeedCompareExtension(str1)
+        val needCompareExt2 = isNeedCompareExtension(str2)
+
+        return when {
+            needCompareExt1 && needCompareExt2 -> compareFileNameAndExt(str1, str2)
+            needCompareExt1.not() && needCompareExt2.not() -> compareText(str1, str2)
+            needCompareExt1 -> 1
+            else -> -1
+        }
+    }
+
+    /**
+     * 比较文件名，文件名相同时返回文件后缀比较结果
+     */
+    private fun compareFileNameAndExt(str1: String, str2: String): Int {
+        //进入前已确保index有效
+        val separatorIndex1 = str1.indexOf(".")
+        val separatorIndex2 = str2.indexOf(".")
+
+        val fileName1 = str1.substring(0, separatorIndex1)
+        val fileName2 = str2.substring(0, separatorIndex2)
+
+        //优先比较文件名
+        val result = compareText(fileName1, fileName2)
+        if (result != 0) {
+            return result
+        }
+
+        //文件名相同，比较扩展名
+        val extension1 = str1.substring(separatorIndex1)
+        val extension2 = str2.substring(separatorIndex2)
+        return compareText(extension1, extension2)
+    }
+
+    /**
+     * 如果两个字符串都是数字，则作为数字比较
+     * @return 比较结果，null则代表至少有一个字符串不能作为数字比较
+     */
+    private fun compareInt(str1: String, str2: String): Int? {
+        val bigInt1 = str1.toBigIntegerOrNull() ?: return null
+        val bigInt2 = str2.toBigIntegerOrNull() ?: return null
+
+        val result = bigInt1.compareTo(bigInt2)
+        if (result != 0) {
+            return result
+        }
+        //数值相同时，比较字符串长度，较长的在后
+        return str1.length.compareTo(str2.length)
+    }
+
+    /**
+     * 文字比较，删除两个文字前后相同部分后，再进行比较
+     */
+    private fun compareText(str1: String, str2: String): Int {
+        if (str1 == str2)
+            return 0
+
         val diff = getDiffText(str1, str2)
-        return compareDiff(diff.first, diff.second)
-    }
+        val diff1 = diff.first
+        val diff2 = diff.second
 
-    private fun compareDiff(str1: String, str2: String): Int {
-        if (str1.isEmpty()) return -1
-        if (str2.isEmpty()) return 1
+        if (diff1 == diff2) return 0
+        if (diff1.isEmpty()) return -1
+        if (diff2.isEmpty()) return 1
 
-        //字符串都是数字，转换为BigInteger比较
-        if (str1.isDigitsOnly() && str2.isDigitsOnly()){
-            return str1.toBigInteger().compareTo(str2.toBigInteger())
+        //尝试将整体视为Int比较
+        val intCompare = compareInt(diff1, diff2)
+        if (intCompare != null) {
+            return intCompare
         }
-        return str1.compareTo(str2)
+        return diff1.compareTo(diff2)
     }
 
-    private fun getDiffText(str1: String, str2: String): Pair<String, String>{
+    /**
+     * 获取两个文字不相同部分
+     * 通过移除前后相同部分得到
+     */
+    private fun getDiffText(str1: String, str2: String): Pair<String, String> {
         if (str1.isEmpty() || str2.isEmpty())
             return Pair(str1, str2)
 
@@ -94,8 +164,16 @@ class FileComparator<T>(
         }
 
         val diffStr1 = String(diffChar1).trim()
-        val diffStr2= String(diffChar2).trim()
+        val diffStr2 = String(diffChar2).trim()
 
         return Pair(diffStr1, diffStr2)
+    }
+
+    /**
+     * 是否存在需要比较的文件扩展名
+     */
+    private fun isNeedCompareExtension(str: String): Boolean {
+        val separatorIndex = str.indexOf(".")
+        return separatorIndex in 1 until str.length
     }
 }

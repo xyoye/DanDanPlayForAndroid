bfa5f1496cb476dbea84edc9990e3fdab9554c98
xyoye <xieyy@anjiu-tech.com>
2021-12-22 16:16:03 +0800

    [Bug修复](dev_4.0.5): 修复依赖更新检查异常

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.github.ben-manes.versions") version "0.39.0"
+}
+
 allprojects {
     repositories {
         google()
@@ -24,4 +28,23 @@ tasks {
     val clean by registering(Delete::class) {
         delete(buildDir)
     }
+
+    //检查依赖库更新
+    //gradlew dependencyUpdates
+    dependencyUpdates {
+        rejectVersionIf {
+            isNonStable(candidate.version)
+        }
+        checkForGradleUpdate = true
+        outputFormatter = "html"
+        outputDir = "build/dependencyUpdates"
+        reportfileName = "report"
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }

5c8f87f809accc42ec97f4dc524c04e217820001
xyoye <xieyy@anjiu-tech.com>
2021-12-22 15:24:53 +0800

    [新增功能](dev_build_src): 增加检查依赖更新插件

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 repositories {
@@ -10,4 +11,25 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:7.0.4")
     implementation(kotlin("gradle-plugin", "1.5.31"))
+}
+
+tasks {
+    //检查依赖库更新
+    //gradlew -p buildSrc dependencyUpdates
+    dependencyUpdates {
+        rejectVersionIf {
+            isNonStable(candidate.version)
+        }
+        checkForGradleUpdate = true
+        outputFormatter = "html"
+        outputDir = "build/dependencyUpdates"
+        reportfileName = "report"
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }

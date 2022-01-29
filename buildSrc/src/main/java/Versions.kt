61ae0cb2d7b54664d48ef0b27f033709a4e6760b
xyoye <xyoye1997@outlook.com>
2022-01-29 15:49:09 +0800

    package-release

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,6 @@ object Versions {
     const val compileSdkVersion = 31
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
-    const val versionCode = 51
+    const val versionCode = 52
     const val versionName = "4.0.5"
 }

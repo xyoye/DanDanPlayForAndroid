3aa5cc651ec576435d245c434a2a6a24b523b2d3
xieyy <xieyy@anjiu-tech.com>
2022-10-12 14:48:01 +0800

    [功能完善](master): 补充设置图标

--- player_component/src/main/java/com/xyoye/player/info/SettingAction.kt
+++ player_component/src/main/java/com/xyoye/player/info/SettingAction.kt
@@ -1,29 +1,31 @@
 package com.xyoye.player.info
 
+import com.xyoye.player_component.R
+
 enum class SettingAction(val type: SettingActionType, val display: String, val icon: Int) {
-    VIDEO_ASPECT(SettingActionType.VIDEO, "比例", 0),
+    VIDEO_ASPECT(SettingActionType.VIDEO, "比例", R.drawable.ic_setting_video_aspect),
 
-    VIDEO_SPEED(SettingActionType.VIDEO, "倍速", 0),
+    VIDEO_SPEED(SettingActionType.VIDEO, "倍速", R.drawable.ic_setting_video_speed),
 
-    DANMU_LOAD(SettingActionType.DANMU, "装载", 0),
+    DANMU_LOAD(SettingActionType.DANMU, "装载", R.drawable.ic_setting_add_source),
 
-    DANMU_STYLE(SettingActionType.DANMU, "样式", 0),
+    DANMU_CONFIG(SettingActionType.DANMU, "配置", R.drawable.ic_setting_danmu_block),
 
-    DANMU_CONFIG(SettingActionType.DANMU, "配置", 0),
+    DANMU_STYLE(SettingActionType.DANMU, "样式", R.drawable.ic_setting_style),
 
-    DANMU_TIME(SettingActionType.DANMU, "时间", 0),
+    DANMU_TIME(SettingActionType.DANMU, "时间", R.drawable.ic_setting_time),
 
-    SUBTITLE_LOAD(SettingActionType.SUBTITLE, "装载", 0),
+    SUBTITLE_LOAD(SettingActionType.SUBTITLE, "装载", R.drawable.ic_setting_add_source),
 
-    SUBTITLE_STREAM(SettingActionType.SUBTITLE, "字幕轨", 0),
+    SUBTITLE_STREAM(SettingActionType.SUBTITLE, "字幕轨", R.drawable.ic_setting_subtitle_stream),
 
-    SUBTITLE_STYLE(SettingActionType.SUBTITLE, "样式", 0),
+    SUBTITLE_STYLE(SettingActionType.SUBTITLE, "样式", R.drawable.ic_setting_style),
 
-    SUBTITLE_TIME(SettingActionType.SUBTITLE, "时间", 0),
+    SUBTITLE_TIME(SettingActionType.SUBTITLE, "时间", R.drawable.ic_setting_time),
 
-    VIDEO_STREAM(SettingActionType.OTHER, "音轨", 0),
+    VIDEO_STREAM(SettingActionType.OTHER, "音轨", R.drawable.ic_setting_audio_stream),
 
-    SCREEN_ORIENTATION(SettingActionType.OTHER, "屏幕翻转", 0),
+    SCREEN_ORIENTATION(SettingActionType.OTHER, "屏幕翻转", R.drawable.ic_setting_rotate),
 
-    NEXT_EPISODE(SettingActionType.OTHER, "自动换集", 0),
+    NEXT_EPISODE(SettingActionType.OTHER, "顺序播放", R.drawable.ic_setting_order_play),
 }

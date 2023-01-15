92525c032eee7de4fc9298cdfe48c71ce44ad81d
xyoye <xyoye1997@outlook.com>
2023-01-15 19:07:40 +0800

    [代码优化](dev_storage_file): 调整查询语句写法

--- common_component/src/main/java/com/xyoye/common_component/database/dao/PlayHistoryDao.kt
+++ common_component/src/main/java/com/xyoye/common_component/database/dao/PlayHistoryDao.kt
@@ -24,14 +24,12 @@ interface PlayHistoryDao {
     @TypeConverters(MediaTypeConverter::class)
     suspend fun gitLastPlay(vararg mediaTypes: MediaType): PlayHistoryEntity?
 
-    @Query("SELECT history.*,(last_play.unique_key IS NOT NULL) as is_last_play" +
-            " FROM (" +
-            "SELECT * FROM play_history WHERE unique_key = (:uniqueKey) AND media_type = (:mediaType)" +
-            ") as history" +
-            " LEFT JOIN (" +
-            "SELECT unique_key FROM play_history WHERE media_type = (:mediaType) ORDER BY play_time DESC LIMIT 1" +
-            ") as last_play" +
-            " ON history.unique_key == last_play.unique_key")
+    @Query(
+        "SELECT history.*,(last_play.unique_key IS NOT NULL) as is_last_play" +
+            " FROM (SELECT * FROM play_history WHERE unique_key = (:uniqueKey) AND media_type = (:mediaType)) as history" +
+            " LEFT JOIN (SELECT unique_key FROM play_history WHERE media_type = (:mediaType) ORDER BY play_time DESC LIMIT 1) as last_play" +
+            " ON history.unique_key == last_play.unique_key"
+    )
     @TypeConverters(MediaTypeConverter::class)
     suspend fun getPlayHistory(uniqueKey: String, mediaType: MediaType): PlayHistoryEntity?
 

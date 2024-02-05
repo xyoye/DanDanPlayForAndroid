71fe551beb62c94ca1d5a62fe139a79be38e89f6
xyoye <xyoye1997@outlook.com>
2024-02-05 16:20:16 +0800

    其它提交: 初始化4.2.0分支

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,6 @@ object Versions {
     const val compileSdkVersion = 33
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
-    const val versionCode = 57
-    const val versionName = "4.1.0"
+    const val versionCode = 58
+    const val versionName = "4.2.0"
 }

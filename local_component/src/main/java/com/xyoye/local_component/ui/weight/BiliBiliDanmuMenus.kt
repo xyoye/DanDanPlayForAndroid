365e24b0cb75db7a0ed8d2b00d47fcedf2e03059
xieyy <xieyy@anjiu-tech.com>
2024-09-02 15:56:27 +0800

    新增功能: 新增B站弹幕下载自定义User-Agent

---
+++ local_component/src/main/java/com/xyoye/local_component/ui/weight/BiliBiliDanmuMenus.kt
@@ -0,0 +1,48 @@
+package com.xyoye.local_component.ui.weight
+
+import android.view.Menu
+import android.view.MenuItem
+import com.xyoye.common_component.config.AppConfig
+import com.xyoye.common_component.weight.dialog.CommonEditDialog
+import com.xyoye.data_component.bean.EditBean
+import com.xyoye.local_component.R
+import com.xyoye.local_component.ui.activities.bilibili_danmu.BilibiliDanmuActivity
+
+/**
+ *    author: xieyy@anjiu-tech.com
+ *    time  : 2024/9/2
+ *    desc  :
+ */
+class BiliBiliDanmuMenus private constructor(
+    private val activity: BilibiliDanmuActivity
+) {
+
+    companion object {
+        fun inflater(activity: BilibiliDanmuActivity, menu: Menu): BiliBiliDanmuMenus {
+            activity.menuInflater.inflate(R.menu.menu_bilibili_danmu, menu)
+            return BiliBiliDanmuMenus(activity)
+        }
+    }
+
+    fun onOptionsItemSelected(item: MenuItem) {
+        if (item.itemId == R.id.item_modify_user_agent) {
+            showModifyUserAgentDialog()
+            return
+        }
+    }
+
+    private fun showModifyUserAgentDialog() {
+        CommonEditDialog(
+            activity,
+            EditBean(
+                "修改User-Agent",
+                "User-Agent不应为空",
+                "请输入User-Agent",
+                defaultText = AppConfig.getJsoupUserAgent(),
+                inputTips = "使用【选取链接下载】时，会携带此User-Agent请求链接的网页内容"
+            ),
+        ) {
+            AppConfig.putJsoupUserAgent(it)
+        }.show()
+    }
+}

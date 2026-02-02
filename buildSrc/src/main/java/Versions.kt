83d39604fd1c6cb771b0faeb781e42937320a8d1
xieyy <xieyy@anjiu-tech.com>
2026-02-02 16:56:18 +0800

    其他提交: 初始化4.2.0分支

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,6 @@ object Versions {
     const val compileSdkVersion = 33
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
-    const val versionCode = 60
-    const val versionName = "4.1.3"
+    const val versionCode = 61
+    const val versionName = "4.2.0"
 }

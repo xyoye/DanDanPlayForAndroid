2c2ef822f4267cca4d5886929fc654c6cb7f50b3
xieyy <xieyy@anjiu-tech.com>
2026-02-02 14:39:33 +0800

    release-4.1.3

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,6 @@ object Versions {
     const val compileSdkVersion = 33
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
-    const val versionCode = 59
-    const val versionName = "4.1.2"
+    const val versionCode = 60
+    const val versionName = "4.1.3"
 }
